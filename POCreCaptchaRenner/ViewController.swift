ab267d315a24d994dc7c0843b2af7d3169ad3d73
Ricardo Caldeira de Andrade <ricardo.andrade@meta.com.br>
2021-12-17 10:12:54 -0300

    Initial Commit

---
+++ POCreCaptchaRenner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POCreCaptchaRenner
+//
+//  Created by Ricardo Caldeira on 17/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
